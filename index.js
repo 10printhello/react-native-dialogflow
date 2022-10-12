bd9be12a143d8119b2eec23aaffc957a302f0d45
Stuart <stuart@foodlum.co.ul>
2022-10-12 11:28:31 +0100

    removed native items around Voice so we can use with Expo

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ import { Dialogflow_V3 } from './js/Dialogflow_V3';
 var dialogflow3 = new Dialogflow_V3();
 
 dialogflow3.setConfiguration = async function (clientEmail, privateKey, languageTag, projectId, locationId, agentId) {
-    dialogflow3.accessToken = await dialogflow2.generateAccessToken(clientEmail, privateKey);
+    dialogflow3.accessToken = await dialogflow3.generateAccessToken(clientEmail, privateKey);
     dialogflow3.languageTag = languageTag;
     dialogflow3.projectId = projectId;
-    dialogflow3.sessionId = dialogflow2.sessionId ? dialogflow2.sessionId : dialogflow2.guid();
+    dialogflow3.sessionId = dialogflow3.sessionId ? dialogflow3.sessionId : dialogflow3.guid();
     dialogflow3.agentId = agentId;
     dialogflow3.locationId = locationId;
     dialog
