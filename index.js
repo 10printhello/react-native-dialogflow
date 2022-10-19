2563436c88b30efb04322cade198c7c1e1d1bd57
10printhello <stuart.a.mainwaring@gmail.com>
2022-10-19 10:44:42 +0100

    Added Dialogflow_V2 as well as V3 to index.js

--- index.js
+++ index.js
@@ -17,5 +17,16 @@ dialogflow3.setConfiguration = async function (clientEmail, privateKey, language
     dialogflow3.locationId = locationId;
 }
 
-export { dialogflow3 as Dialogflow_V3 };
+/**
+ * DIALOGFLOW V2
+ */
+var dialogflow2 = new Dialogflow_V2();
 
+dialogflow2.setConfiguration = async function (clientEmail, privateKey, languageTag, projectId) {
+    dialogflow2.accessToken = await dialogflow2.generateAccessToken(clientEmail, privateKey);
+    dialogflow2.languageTag = languageTag;
+    dialogflow2.projectId = projectId;
+    dialogflow2.sessionId = dialogflow2.sessionId ? dialogflow2.sessionId : dialogflow2.guid();
+}
+
+export { dialogflow3 as Dialogflow_V3, dialogflow2 as Dialogflow_V2 };
